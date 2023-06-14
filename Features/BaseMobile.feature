521c25ead2b1692f5e099cb0ed62532c85f8aaca
Sharon Malone <sharon.malone.rp@viaexperis2.pe>
2023-06-14 11:37:57 -0500

    WorkFlowMarketplaceMobile base

---
+++ Features/BaseMobile.feature
@@ -0,0 +1,4 @@
+﻿Feature: Marketplace Mobile
+
+Scenario: Flujo de Registro con Codigo de Acceso
+	Given El usuario selecciona opcion registrarme
---
+++ Variables/ConfigVariables.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WorkFlowMarketplaceMobile.Variables
+{
+    class ConfigVariables
+    {
+        public string BasePathApk { get; set; }
+        public string BaseUriAppium { get; set; }
+        public string BaseDeviceName { get; set; }
+        public int BaseTimeWait { get; set; }
+        public int BaseCommandTimeout { get; set; }
+    }
+
+}
